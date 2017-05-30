67fe5f73643fbf47100d54432820e20655c16051
Seungha Lee <poopa8788@gmail.com>
2017-05-30 14:29:36 +0900

    add post_create view, url, template

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -7,14 +7,14 @@ from .models import Post
 def post_list(request):
     # return HttpResponse('<html><body>Post List</body></html>')
     # posts변수에 DRM을 이용해서 전체 Post의 리스트(쿼리셋)을 대입
-    # posts = Post.objects.all()
+    posts = Post.objects.all()
 
     # posts변수에 DRM을 사용해서 전달할 쿼리셋이
     # Post의 published_date가 timezone.now()보다
     # 작은값을 가질때만 해당하도록 필터를 사용한다.
-    posts = Post.objects.filter(
-        published_date__lte=timezone.now()
-    )
+    # posts = Post.objects.filter(
+    #     published_date__lte=timezone.now()
+    # )
     context = {
         'title': 'PostList from post_list view',
         'posts':posts,
@@ -30,3 +30,9 @@ def post_detail(request, pk):
         'post': Post.objects.get(pk=pk),
     }
     return render(request, 'blog/post_detail.html', context)
+
+def post_create(request):
+    context = {
+
+    }
+    return render(request, 'blog/post_create.html', context)
